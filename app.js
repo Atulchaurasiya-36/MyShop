1dc7b082bdb095c9333156b2ae996f014905f9e2
Atulchaurasiya-36 <atulchaurasiya66602@gmail.com>
2024-04-26 21:36:03 +0530

    first push

---
+++ app.js
@@ -0,0 +1,8 @@
+let button = document.querySelector("#view");
+button.addEventListener("click", () => {
+    let women=document.querySelector(".womens1")
+    women.style.display="block"
+   women.style.display="flex"
+
+});
+
